9093083ecd70659509ae5efb9e01890038edb76a
Todd Hudgens <todd.hudgens@gmail.com>
2018-04-04 23:46:34 -0400

    Adding new step for creating twig cache directory

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ read -p "MySQL Root Username: " MYSQL_UN
 rm ~/.mylogin.cnf > /dev/null 2>&1
 mysql_config_editor set --login-path=local --host=localhost --user=$MYSQL_UN --password
 mysql --login-path=local -e "CREATE database my_financials;" > /dev/null 2>&1
-mysql --login-path=local -e "SOURCE /var/www/sql/schema.sql;"
+mysql --login-path=local -e "SOURCE ./sql/schema.sql;"
 
 MYSQLUSERPW="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)"
 printf "[DONE]\n\n"
@@ -29,7 +29,11 @@ composer install
 cd ../
 printf "[DONE]\n\n"
 
-printf "5. Create your login user\n"
+printf "5. Creating twig cache directory\n"
+mkdir cache
+printf "[DONE]\n\n"
+
+printf "6. Create your login user\n"
 read -p "Username: " username
 while true; do
     read -s -p "Password: " password
@@ -42,7 +46,7 @@ done
 mysql --login-path=local -e "DELETE FROM my_financials.users"
 mysql --login-path=local -e "INSERT INTO my_financials.users VALUES(0,\"$username\",md5(\"$password&CASH_RULES_EVERYTHING_AROUND_ME_CREAM\"));"
 
-printf "\n6. Create an AlphaVanta API key (optional)\n"
+printf "\n7. Create an AlphaVanta API key (optional)\n"
 printf "  - If you want to use the stock / mutual fund features, you'll need an API key to get pricing data\n"
 printf "  - Sign up for an API key at https://www.alphavantage.co/support/\n\n"
 read -p "API KEY [hit enter to skip]: " alphavantageapikey
